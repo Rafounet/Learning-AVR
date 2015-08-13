3803c4ef3e1cc43cc7af1f3249be82978d5bddf5
Pedro Vinicius <pedrobr1998@gmail.com>
2015-08-13 00:09:49 -0300

    Update and rename mega_shift_register.c to avr_shift_register.c

---
+++ avr_shift_register.c
@@ -0,0 +1,56 @@
+/*
+ * avr_shift_register.c
+ *
+ * Created: 17/12/2014 16:47:24
+ *  Author: Pedro
+ * Update:  13/08/2015 00:05:30
+ *
+ * Exodus 15:18
+ */ 
+#define F_CPU 16000000
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define DATA  		(_BV(PB0)) 
+#define CLOCK 		(_BV(PB1))
+#define LATCH 		(_BV(PB2))
+#define working 	(_BV(PB3))
+
+void send_byte(char b);
+
+uint8_t bytes;
+uint8_t k;
+uint8_t shifted = 0b10000000;
+uint8_t h;
+
+int main(void){
+	DDRB |= CLOCK | DATA | LATCH | working;
+	PORTB |= working; // For put high !MR pin 10 on 595.
+    while(1){
+		for(h=0;h<255;h++){
+			send_byte(bytes);
+			_delay_ms(200);
+			bytes++;
+		}
+	}
+}
+
+void send_byte(char b){
+	shifted = 0b10000000;
+	for (k=0;k<8;k++){
+		PORTB &=~CLOCK;
+		
+		PORTB = (b & shifted)?(PORTB|DATA):(PORTB&(~DATA)); 
+		
+		_delay_loop_1(1);
+		PORTB |= CLOCK;
+		_delay_loop_1(1);
+		PORTB &=~DATA;
+		shifted = shifted >> 1;
+		
+		}
+	PORTB |= LATCH;
+	_delay_loop_2(0);
+	PORTB &= ~LATCH;
+	_delay_loop_2(0);
+}
